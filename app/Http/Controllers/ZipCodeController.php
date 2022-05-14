909684a790f15bcf1bc5a5b8fd8ed4d5f629b873
Aeon1 <joel.r@live.com.mx>
2022-05-13 23:10:31 -0500

    quitar acentos y hacer mayusculas

--- app/Http/Controllers/ZipCodeController.php
+++ app/Http/Controllers/ZipCodeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\zipCode;
-use DB;
+use App\clasesCustom\Helper;
 
 class ZipCodeController extends Controller
 {
@@ -11,13 +11,17 @@ class ZipCodeController extends Controller
         if(strlen($zip_code) == 5){
             $code =zipCode::where("zip_code" , $zip_code)->get();
             if(count($code) != 0){
-                $codex= json_encode($code[0], JSON_UNESCAPED_UNICODE); 
-                $pattern = array("/".$code[0]['locality']."/", "/".$code[0]['settlements'][0]['zone_type']."/");
-                $replace = array(mb_convert_case($code[0]['locality'], MB_CASE_UPPER, "UTF-8"), mb_convert_case($code[0]['settlements'][0]['zone_type'], MB_CASE_UPPER, "UTF-8"));
+                $codex= json_encode($code[0], JSON_UNESCAPED_UNICODE);                
+                $pattern = array("/".$code[0]['locality']."/", "/".$code[0]['federal_entity']['name']."/", "/".$code[0]['settlements'][0]['zone_type']."/");
+                $replace = array(
+                    mb_convert_case($code[0]['locality'], MB_CASE_UPPER, "UTF-8"),
+                    mb_convert_case($code[0]['federal_entity']['name'], MB_CASE_UPPER, "UTF-8"), 
+                    mb_convert_case($code[0]['settlements'][0]['zone_type'], MB_CASE_UPPER, "UTF-8")
+                );
                 $resultado =preg_replace($pattern, $replace, $codex, -1 );
-                return $resultado;
+                return Helper::quitarAcentos($resultado);
             }else{
-                return json_encode(['Error'=> 'CP no encontrado']);
+                return json_encode(['Error'=> 'CP no encontrado'], JSON_UNESCAPED_UNICODE);
             }
         }else{
             return json_encode(['Error'=> 'CP no valido, debe contener 5 dígitos'], JSON_UNESCAPED_UNICODE);
